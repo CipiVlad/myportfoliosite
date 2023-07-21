63dbf8b2effa999f779bf5a81115267783d7ff2d
Ciprian Vlad <supercoder.cipi@gmail.com>
2023-07-21 02:03:55 +0200

    added spinner

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { data } from "../data"
+import GrowSpinner from "../components/GrowSpinner"
+
 const Projects = () => {
     const [projectInfo, setProjectInfo] = useState(data)
     // // const [toolIcons, setToolIcons] = useState([])
@@ -29,37 +31,45 @@ const Projects = () => {
 
             <div class="container" >
                 <div class="row">
-                    {projectInfo.map((e, index) => (
+                    {
 
-                        <div class="col-md-12" key={index} style={{ marginBottom: "14vh" }}>
-                            <div class="d-flex justify-content-center align-items-center">
-                                <div class="card" style={{ width: "45rem" }}>
-                                    <img
-                                        class="class-img-top"
-                                        src={e.image}
-                                        alt={e.title}
-                                    />
-                                    <div class="card-body">
-                                        <h4 class="card-title">{e.title}</h4>
-                                        <p class="card-text">{e.description}</p>
-                                    </div>
-                                    <div class="card-footer">
-                                        <Link
-                                            to={e.gitlink}
-                                            class="btn"
-                                            style={{ display: 'block', textAlign: 'center' }}
-                                        >
+                        projectInfo ?
+                            (projectInfo.map((e, index) => (
+
+                                <div class="col-md-12" key={index} style={{ marginBottom: "14vh" }}>
+                                    <div class="d-flex justify-content-center align-items-center">
+                                        <div class="card" style={{ width: "45rem" }}>
                                             <img
-                                                src="icons/github-mark-white.png"
-                                                alt="" width={50}
+                                                class="class-img-top"
+                                                src={e.image}
+                                                alt={e.title}
                                             />
-                                        </Link>
-                                    </div>
+                                            <div class="card-body">
+                                                <h4 class="card-title">{e.title}</h4>
+                                                <p class="card-text">{e.description}</p>
+                                            </div>
+                                            <div class="card-footer">
+                                                <Link
+                                                    to={e.gitlink}
+                                                    class="btn"
+                                                    style={{ display: 'block', textAlign: 'center' }}
+                                                >
+                                                    <img
+                                                        src="icons/github-mark-white.png"
+                                                        alt="" width={50}
+                                                    />
+                                                </Link>
+                                            </div>
 
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    ))}
+                            ))
+                            )
+                            :
+                            (<GrowSpinner />)
+                    }
+
                 </div>
             </div>
 
