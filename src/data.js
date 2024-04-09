ad62f409fd31a873eba744d3ae44d1f7832a7aec
CipiVlad <supercoder.cipi@gmail.com>
2024-04-09 09:11:23 +0200

    rearange projects

--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
 export const data =
     [
+
+
         {
             "id": 0,
             "title": "bookingNotes",
@@ -14,68 +16,68 @@ export const data =
             ],
             "image": "images/bookingNotes/bookingNotes.png"
         },
-        {
-            "id": 1,
-            "title": "Vanilla TypeScript List",
-            "description": "Small Typescript project. A code along with Dave Gray.",
-            "link": "",
-            "gitlink": "https://github.com/CipiVlad/typescript_vanilla_simpleNotes.git",
-            "tools": [
-                "React",
-                "TypeScript",
-                "express",
-                "jsonwebtoken",
-                "MongoDB"
-            ],
-            "image": "images/typescript_list.png"
-        },
+        // {
+        //     "id": 1,
+        //     "title": "Vanilla TypeScript List",
+        //     "description": "Small Typescript project. A code along with Dave Gray.",
+        //     "link": "",
+        //     "gitlink": "https://github.com/CipiVlad/typescript_vanilla_simpleNotes.git",
+        //     "tools": [
+        //         "React",
+        //         "TypeScript",
+        //         "express",
+        //         "jsonwebtoken",
+        //         "MongoDB"
+        //     ],
+        //     "image": "images/typescript_list.png"
+        // },
+        // {
+        //     "id": 2,
+        //     "title": "Expense Tracker",
+        //     "description": "This is a expense, saving and investment tracker displaying result in a pie chart.",
+        //     "link": "",
+        //     "gitlink": "https://github.com/CipiVlad/expense_saving_tracker.git",
+        //     "tools": [
+        //         "React",
+        //         "Tailwind",
+        //         "chart.js",
+        //         "@reduxjs/toolkit",
+        //         "react-redux",
+        //         "MongoDB"
+        //     ],
+        //     "image": "images/Screenshot_Expense_Tracker.png"
+        // },
+        // {
+        //     "id": 3,
+        //     "title": "Beer API",
+        //     "description": "Here we have collection of beers...fetching juicy data from an API.",
+        //     "link": "https://62abafa2520be74377b40a8d--candid-otter-c6c4d9.netlify.app",
+        //     "gitlink": "https://github.com/CipiVlad/react_beer_API.git",
+        //     "tools": [
+        //         "React",
+        //         "",
+        //         "",
+        //         "",
+        //         ""
+        //     ],
+        //     "image": "images/beerLandingpage.png"
+        // },
+        // {
+        //     "id": 4,
+        //     "title": "MERN Auth - HttpOnly",
+        //     "description": "Store JWT in HttpOnly-Cookie. Code along with TraversyMedia.",
+        //     "link": "https://mern-auth-frontend-777q.onrender.com/",
+        //     "gitlink": "https://github.com/CipiVlad/mern-auth.git",
+        //     "tools": [
+        //         "React",
+        //         "express",
+        //         "jsonwebtoken",
+        //         "MongoDB"
+        //     ],
+        //     "image": "images/MERNAuth.png"
+        // },
         {
             "id": 2,
-            "title": "Expense Tracker",
-            "description": "This is a expense, saving and investment tracker displaying result in a pie chart.",
-            "link": "",
-            "gitlink": "https://github.com/CipiVlad/expense_saving_tracker.git",
-            "tools": [
-                "React",
-                "Tailwind",
-                "chart.js",
-                "@reduxjs/toolkit",
-                "react-redux",
-                "MongoDB"
-            ],
-            "image": "images/Screenshot_Expense_Tracker.png"
-        },
-        {
-            "id": 3,
-            "title": "Beer API",
-            "description": "Here we have collection of beers...fetching juicy data from an API.",
-            "link": "https://62abafa2520be74377b40a8d--candid-otter-c6c4d9.netlify.app",
-            "gitlink": "https://github.com/CipiVlad/react_beer_API.git",
-            "tools": [
-                "React",
-                "",
-                "",
-                "",
-                ""
-            ],
-            "image": "images/beerLandingpage.png"
-        },
-        {
-            "id": 4,
-            "title": "MERN Auth - HttpOnly",
-            "description": "Store JWT in HttpOnly-Cookie. Code along with TraversyMedia.",
-            "link": "https://mern-auth-frontend-777q.onrender.com/",
-            "gitlink": "https://github.com/CipiVlad/mern-auth.git",
-            "tools": [
-                "React",
-                "express",
-                "jsonwebtoken",
-                "MongoDB"
-            ],
-            "image": "images/MERNAuth.png"
-        },
-        {
-            "id": 5,
             "title": "Shopping Products",
             "description": "Basic Shopping List with Detail Page. Store handling with Redux.",
             "link": "",
@@ -116,47 +118,48 @@ export const data =
             ],
             "image": "images/cli_prompt_qrcode.png"
         },
-        {
-            "id": 7,
-            "title": "The Wiki - API. NextJS with TypeScript",
-            "description": "Dave Gray NextJS Course.",
-            "link": "",
-            "gitlink": "https://github.com/CipiVlad/dave_gray_nextjs_course/tree/main/small_project/next-ts-small-project",
-            "tools": [
-                "NextJS",
-                "TypeScript",
-                "Tailwind",
-                "",
-                ""
-            ],
-            "image": "images/nextjs_wikirocket_home.png"
-        },
-        {
-            "id": 8,
-            "title": "techNotes",
-            "description": "An awsome MERN-Stack Project created by Dave Gray, including JSON Web Token, Redux Toolkit and many useful features.",
-            "link": "",
-            "gitlink": "https://github.com/CipiVlad/techNotes_MERN-Stack_React_Redux_RTK-Query.git",
-            "tools": [
-                "React",
-                "express",
-                "jsonwebtoken",
-                "MongoDB"
-            ],
-            "image": "images/technotes.png"
-        },
-        {
-            "id": 9,
-            "title": "TypeScript/React Todo App",
-            "description": "A FreeCodeCamp Project using Framer Motion and LocalStorage.",
-            "link": "",
-            "gitlink": "https://github.com/CipiVlad/ts_react_freeCodeCamp_todo.git",
-            "tools": [
-                "React",
-                "TypeScript",
-                "Framer Motion",
-                "Local Storage"
-            ],
-            "image": "images/ts_react_todo.png"
-        },
+        // {
+        //     "id": 7,
+        //     "title": "The Wiki - API. NextJS with TypeScript",
+        //     "description": "Dave Gray NextJS Course.",
+        //     "link": "",
+        //     "gitlink": "https://github.com/CipiVlad/dave_gray_nextjs_course/tree/main/small_project/next-ts-small-project",
+        //     "tools": [
+        //         "NextJS",
+        //         "TypeScript",
+        //         "Tailwind",
+        //         "",
+        //         ""
+        //     ],
+        //     "image": "images/nextjs_wikirocket_home.png"
+        // },
+        // {
+        //     "id": 8,
+        //     "title": "techNotes",
+        //     "description": "An awsome MERN-Stack Project created by Dave Gray, including JSON Web Token, Redux Toolkit and many useful features.",
+        //     "link": "",
+        //     "gitlink": "https://github.com/CipiVlad/techNotes_MERN-Stack_React_Redux_RTK-Query.git",
+        //     "tools": [
+        //         "React",
+        //         "express",
+        //         "jsonwebtoken",
+        //         "MongoDB"
+        //     ],
+        //     "image": "images/technotes.png"
+        // },
+        // {
+        //     "id": 9,
+        //     "title": "TypeScript/React Todo App",
+        //     "description": "A FreeCodeCamp Project using Framer Motion and LocalStorage.",
+        //     "link": "",
+        //     "gitlink": "https://github.com/CipiVlad/ts_react_freeCodeCamp_todo.git",
+        //     "tools": [
+        //         "React",
+        //         "TypeScript",
+        //         "Framer Motion",
+        //         "Local Storage"
+        //     ],
+        //     "image": "images/ts_react_todo.png"
+        // },
+
     ]
